61b98f2fe1aed833354bdd223bd6b92f861f1835
Algiane Froehly <algiane@gmail.com>
2024-03-12 11:20:59 +0100

    Test self-hosted runner.

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "coucou"
+echo -e "Display the architecture of target machine wherein we wanted to do ssh\n" 
+arch
+echo -e "Display the cpu details\n"
+lscpu
+echo -e "Display the kernel details\n"
+uname -a
